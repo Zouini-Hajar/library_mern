42ec813f5a6c5c967309a954799c0a9350ce9795
Hajar Zouini <zouinihajar1@gmail.com>
2024-05-22 14:02:58 +0100

    Added cart reducer to store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import booksReducer from "../features/books/booksSlice";
 import userReducer from "../features/user/userSlice";
-import borrowingReducer from "../features/borrowings/borrowingsSlice";
+import borrowingsReducer from "../features/borrowings/borrowingsSlice";
+import cartReducer from "../features/cart/cartSlice";
  
 export default configureStore({
     reducer: {
         books: booksReducer,
         user: userReducer,
-        borrowings: borrowingReducer
+        borrowings: borrowingsReducer,
+        cart: cartReducer
     }
 });
